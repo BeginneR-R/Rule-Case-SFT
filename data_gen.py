e285a9ccfa2cf7d0a387dd21f2a10a6e9b55ff23
Blahhccc <2464878765@qq.com>
2025-05-27 19:13:17 +0800

    重新生成DF_step02

--- data_gen.py
+++ data_gen.py
@@ -17,10 +17,13 @@ from gpt_api import nl2logic, logic2coverage, coverage2nl, nl2instruction
 
 data_path_01 = "data/DF/rules1_type.json"
 written_path_01 = "data/DF/DF_step01.json"
+
 data_path_02 = written_path_01
 written_path_02 = "data/DF/DF_step02.json"
+
 data_path_03 = written_path_02
 written_path_03 = "data/DF/DF_step03.json"
+
 data_path_04 = written_path_03
 written_path_04 = "data/DF/DF_step04.json"
 
@@ -28,10 +31,10 @@ written_path_04 = "data/DF/DF_step04.json"
 # nl2logic(data_path_01, written_path_01)
 
 # 执行step02：逻辑表达式根据覆盖率取值
-# logic2coverage(data_path_02, written_path_02)
+logic2coverage(data_path_02, written_path_02)
 
 # 执行step03：逻辑表达式转化为自然语言
 # coverage2nl(data_path_03, written_path_03)
 
 # 执行step04：自然语言转化为指令
-nl2instruction(data_path_04, written_path_04)
+# nl2instruction(data_path_04, written_path_04)
